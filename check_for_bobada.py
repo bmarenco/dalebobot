98578a14571238c3318b852d2815e2ac5a4fb7ff
bmarenco <bernardomarenco@gmail.com>
2020-05-26 11:36:10 -0300

    Added new friends check rate parameter for custom stream listener. Minor style corrections

--- check_for_bobada.py
+++ check_for_bobada.py
@@ -8,10 +8,11 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 class BobadaStreamListener(tweepy.StreamListener):
-    def __init__(self, api, bobo_list):
+    def __init__(self, api, bobo_list, new_friends_check_rate=5):
         self.api = api
         self.bobo_list = bobo_list
         self.last_new_friends_check = time.time()
+        self.new_friends_check_rate = new_friends_check_rate*60
 
     def on_status(self,tweet):
         logger.info("Processing tweet id %d" % (tweet.id))
@@ -19,43 +20,41 @@ class BobadaStreamListener(tweepy.StreamListener):
         #Check if tweet author is in list
         if tweet.author.id_str not in self.bobo_list:
             logger.info("Tweet was authored by %s" %(tweet.author.screen_name))
-            return self.check_new_follows() 
         
         # If it's a reply, do nothing
-        if tweet.in_reply_to_status_id is not None:
+        elif tweet.in_reply_to_status_id is not None:
             logger.info("Tweet was reply")
-            return self.check_new_follows() 
         
         # If it's a retweet, do nothing
-        if hasattr(tweet, 'retweeted_status'):
+        elif hasattr(tweet, 'retweeted_status'):
             logger.info("Tweet was retweet")
-            return self.check_new_follows() 
 
-        # Reply
-        try:
-            username = tweet.author.screen_name
-            message = "@%s Dale bobo" %username
-            logger.info("Replying to %s" % (username))
-            new_tweet = self.api.update_status(status=message, in_reply_to_status_id=tweet.id)
-            # Retweet response
-            if not new_tweet.retweeted:
-                new_tweet.retweet()
-            
-        except tweepy.TweepError as e:
-            logger.error("Error on reply and retweet: %s" % (e.message[0]['message']), exc_info=True)
+        else:
+            # Reply
+            try:
+                username = tweet.author.screen_name
+                message = "@%s Dale bobo" %username
+                logger.info("Replying to %s" % (username))
+                new_tweet = self.api.update_status(status=message, in_reply_to_status_id=tweet.id)
+                # Retweet response
+                if not new_tweet.retweeted:
+                    new_tweet.retweet()
+                
+            except tweepy.TweepError as e:
+                logger.error("Error on reply and retweet: %s" % (e.message[0]['message']), exc_info=True)
  
-        return self.check_new_follows() 
+        return self.check_new_friends() 
     
     def on_error(self, status):
         logger.error(status,exc_info=True)
         
-    def check_new_follows(self):
+    def check_new_friends(self):
         # Check new follows every 5 minutes
         # This is to avoid reaching rate limit for this stream
         # See https://developer.twitter.com/en/docs/basics/rate-limiting
         new_time = time.time()
         elapsed = new_time - self.last_new_friends_check
-        if (elapsed < 5*60):
+        if (elapsed < self.new_friends_check_rate):
             return True
         else:
             new_follows_id = [friend.id_str for friend in tweepy.Cursor(self.api.friends).items()]
@@ -113,10 +112,11 @@ def main(bobo_list):
         logger.info("OK go")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Bot that replies <<Dale bobo>> to the list of people it follows.")
+    parser = argparse.ArgumentParser(description="Twitter bot that replies <<Dale bobo>> to the list of people it follows.")
     parser.add_argument("-b", "--bobo_list", metavar="bobo_list", type=str, required=False,
                         default=None, nargs='+', action="store",
-                        help="list of bobo usernames you want the bot to follow and reply to. If not given, it only replies to the ones it follows")
+                        help="list ofTwitter handles you want the bot to follow and reply to.\
+                                If not given, it only replies to the ones it already follows")
     
     args = parser.parse_args()
     bobo_list =  args.bobo_list
